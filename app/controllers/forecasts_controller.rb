4e4beba911e7607292a0e718fa07438188e86bda
Vitor Pellegrino <vitorp@gmail.com>
2010-07-21 19:49:57 -0300

    Add cache

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,6 +1,7 @@
 class ForecastsController < ApplicationController
   
   def index
+    expires_in (Time.now.end_of_day - Time.now).seconds, :public => true
     @zoodiacs = Zoodiac.all
     @phrases = Phrase.all
   end

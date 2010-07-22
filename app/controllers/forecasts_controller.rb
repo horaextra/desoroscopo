fba19e5604ba67bf9d5dfdd425a515c87f024425
Rodrigo Yoshima <rodrigoy@gmail.com>
2010-07-22 02:46:48 -0300

    agora não repete frases na mesma página

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -3,8 +3,21 @@ class ForecastsController < ApplicationController
   def index
     #expires_in (Time.now.end_of_day - Time.now).seconds, :public => true
     @zoodiacs = Zoodiac.all
-    @phrases = Phrase.all
+    @phrases = pick_phrases
   end
 
+  protected
+  
+  def pick_phrases
+    
+    all_phrases = Phrase.all
+    return_phrases = []
+
+    @zoodiacs.size.times do
+       return_phrases << all_phrases.delete_at(rand(all_phrases.size + 1))
+    end
+ 
+    return return_phrases
+  end
 end
  
--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -3,5 +3,5 @@ class Phrase
   def self.all
     @phrases ||= YAML.load(File.read("#{RAILS_ROOT}/db/phrases.yml"))
   end
-  
+ 
 end

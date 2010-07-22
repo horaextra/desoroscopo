0bcc26251733c3fb0fde98c756788f648b432ada
Rodrigo Yoshima <rodrigoy@gmail.com>
2010-07-22 04:08:31 -0300

    patch para não repetir frases na mesma tela

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -3,8 +3,15 @@ class ForecastsController < ApplicationController
   def index
     #expires_in (Time.now.end_of_day - Time.now).seconds, :public => true
     @zoodiacs = Zoodiac.all
-    @phrases = Phrase.all
+    
+    # o array.draw(12) não funfa... tem repetição...        
+
+    all_phrases = Phrase.all
+    @phrases = []
+    @zoodiacs.size.times do
+      @phrases << all_phrases.delete(all_phrases[rand(all_phrases.size)])
+    end
   end
 
 end
- 
+

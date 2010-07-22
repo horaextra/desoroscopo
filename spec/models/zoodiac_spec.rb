7c40b4af9219a036e81768b4a246f510d8dbd3a9
Marcos Tapajós <tapajos@gmail.com>
2010-07-22 13:54:13 -0300

    Fix zoodiacs order

--- spec/models/zoodiac_spec.rb
+++ spec/models/zoodiac_spec.rb
@@ -5,7 +5,7 @@ describe Zoodiac do
   describe "#all" do
 
     it "should return all" do
-      expected = ["Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem", "Libra", "Escorpião", "Sagitário", "Capricórnio", "Aquário", "Peixes"]
+      expected = ["Capricórnio", "Aquário", "Peixes", "Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem", "Libra", "Escorpião", "Sagitário"]
       Zoodiac.all.should == expected
     end
 

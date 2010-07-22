f05e3e6f2a3f2e674ffa2944163505a723599a41
Rodrigo Yoshima <rodrigoy@gmail.com>
2010-07-22 03:03:12 -0300

    agora não repete frases na mesma página

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -3,21 +3,16 @@ class ForecastsController < ApplicationController
   def index
     #expires_in (Time.now.end_of_day - Time.now).seconds, :public => true
     @zoodiacs = Zoodiac.all
-    @phrases = pick_phrases
-  end
-
-  protected
-  
-  def pick_phrases
     
-    all_phrases = Phrase.all
-    return_phrases = []
+    # o array.draw(12) não funfa... tem repetição... código tosco:
+    # http://github.com/tomas-stefano/arrayextension/blob/master/lib/arrayextension.rb       
 
+    all_phrases = Phrase.all
+    @phrases = []
     @zoodiacs.size.times do
-       return_phrases << all_phrases.delete_at(rand(all_phrases.size + 1))
+      @phrases << all_phrases.delete(all_phrases[rand(all_phrases.size)])
     end
- 
-    return return_phrases
   end
+
 end
  

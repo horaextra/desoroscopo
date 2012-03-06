0514641d3e8824d24f9ff6af49d872357bf1de57
Andre Fonseca <aoqfonseca@gmail.com>
2012-03-05 21:21:09 -0300

    retirando espaço em branco dos arquivos

--- models/phrases.rb
+++ models/phrases.rb
@@ -1,18 +1,18 @@
 class Phrases < Array
-  
+
   def initialize
     replace YAML.load_file(File.dirname(__FILE__) + "/../db/phrases.yml")
     @drawn = []
   end
-  
+
   def select
     result = candidates.draw.first
     @drawn << result
     result
   end
-  
+
   def candidates
     self - @drawn
   end
-  
+
 end
